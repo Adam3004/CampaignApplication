63290921ec17e7209f9f4ff68f2139b5743f37a3
Adam <Adam.ja.com@op.pl>
2022-05-21 20:15:30 +0200

    Addition of SellerController and Seller Service and getSellers method

---
+++ src/main/java/pl/campaignapplication/controller/SellerController.java
@@ -0,0 +1,21 @@
+package pl.campaignapplication.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.campaignapplication.seller.Seller;
+import pl.campaignapplication.service.SellerService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class SellerController {
+    private final SellerService sellerService;
+
+    @GetMapping("/towns")
+    public List<Seller> getSellers(){
+        return sellerService.getSellers();
+    }
+
+}
