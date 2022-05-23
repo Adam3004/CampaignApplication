42798fc964d40b1d0fe0f95cc30a8138281d884c
Adam <Adam.ja.com@op.pl>
2022-05-23 12:50:44 +0200

    Primary frontend for crud. Need some correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import { Component } from 'react';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CampaignEdit from './CampaignEdit';
+import CampaignList from './CampaignList';
 
 // function App() {
 //   return (
@@ -25,39 +29,56 @@ import { Component } from 'react';
 
 // export default App;
 
+// class App extends Component {
+//   state = {
+//     campaigns: []
+//   };
+
+//   async componentDidMount() {
+//     const response = await fetch('/campaigns');
+//     const body = await response.json();
+//     this.setState({campaigns: body});
+//   }
+
+//   render() {
+//     const {campaigns} = this.state;
+//     return (
+//         <div className="Application">
+//           <header className="App-header">
+//             <img src={logo} className="App-logo" alt="logo" />
+//             <div className="App-intro">
+//               <h2>Campaigns</h2>
+//               {campaigns.map(campaign =>
+//                   <div key={campaign.id}>
+//                     Name: {campaign.name} <b></b>
+//                     Bid amount: {campaign.bidAmount} <b></b>
+//                     Bilance: {campaign.campaignFunds} <b></b>
+//                     Status: {campaign.status ? 'on':'off'} <b></b>
+//                     Town: {campaign.town} <b></b>
+//                     Radius: {campaign.radius} <b></b>
+//                     Keywords: {campaign.keywords ? campaign.keywords:"empty"} <b></b>
+//                   </div>
+//               )}
+//             </div>
+//           </header>
+//         </div>
+//     );
+//   }
+// }
+// export default App;
+
 class App extends Component {
-  state = {
-    campaigns: []
-  };
-
-  async componentDidMount() {
-    const response = await fetch('/campaigns');
-    const body = await response.json();
-    this.setState({campaigns: body});
-  }
-
   render() {
-    const {campaigns} = this.state;
     return (
-        <div className="Application">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <div className="App-intro">
-              <h2>Campaigns</h2>
-              {campaigns.map(campaign =>
-                  <div key={campaign.id}>
-                    Name: {campaign.name} <b></b>
-                    Bid amount: {campaign.bidAmount} <b></b>
-                    Bilance: {campaign.campaignFunds} <b></b>
-                    Status: {campaign.status ? 'on':'off'} <b></b>
-                    Town: {campaign.town} <b></b>
-                    Radius: {campaign.radius} <b></b>
-                  </div>
-              )}
-            </div>
-          </header>
-        </div>
-    );
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/campaigns' exact={true} component={CampaignList}/>
+          <Route path='/campaigns/:id' component={CampaignEdit}/>
+        </Switch>
+      </Router>
+    )
   }
 }
+
 export default App;
